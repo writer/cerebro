7cacf4fd2dd81bf14f19fdce294bff9298bb758d
Jonathan Haas <jonathan.haas@writer.com>
2025-10-27 11:18:39 -0700

    feat: add executive analytics route

---
+++ frontend/app/(dashboard)/analytics/executive/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ExecutiveDashboard } from "@/components/analytics/executive-dashboard";
+
+export default function ExecutiveAnalyticsExecutivePage() {
+  return <ExecutiveDashboard />;
+}
