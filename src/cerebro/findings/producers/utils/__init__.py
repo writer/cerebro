9c2471d5aa7fee16fdd94cb5d41866e4a3b151e8
Jonathan Haas <jonathan.haas@writer.com>
2025-11-12 08:05:07 -0800

    style: sort utils exports

--- src/cerebro/findings/producers/utils/__init__.py
+++ src/cerebro/findings/producers/utils/__init__.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 
 __all__ = [
     "analyze_instance_network_exposure",
-    "has_public_interface",
-    "security_group_rule_allows_public",
     "coerce_rule_id",
+    "has_public_interface",
     "resolve_rule_id",
+    "security_group_rule_allows_public",
 ]
 
 from .exposure import (
