b12411aab92a77b98d6647b3aeb19fcf94e36de6
Jonathan Haas <jonathan.haas@writer.com>
2026-02-27 07:43:10 -0800

    feat(identity): expand identity query policy coverage

--- internal/policy/query_policy_guardrail_test.go
+++ internal/policy/query_policy_guardrail_test.go
@@ -2,7 +2,7 @@ package policy
 
 import "testing"
 
-const maxQueryOnlyPoliciesBaseline = 107
+const maxQueryOnlyPoliciesBaseline = 362
 
 func TestQueryOnlyPolicyCountDoesNotGrowBeyondBaseline(t *testing.T) {
 	engine := NewEngine()
