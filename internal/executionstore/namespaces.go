2d2ad74b49935e70351ef8466f8d1afaeaeb0dec
Jonathan Haas <15969068+haasonsaas@users.noreply.github.com>
2026-03-15 17:19:06 -0700

    Add runtime ingest replay and materialization job records (#328)
    * Add runtime ingest replay job records
    
    * Use ListAllRuns for runtime ingest jobs
    
    * Align runtime materialization namespace naming

--- internal/executionstore/namespaces.go
+++ internal/executionstore/namespaces.go
@@ -1,13 +1,15 @@
 package executionstore
 
 const (
-	NamespacePlatformReportRun    = "report_run"
-	NamespaceWorkloadScan         = "workload_scan"
-	NamespaceImageScan            = "image_scan"
-	NamespaceFunctionScan         = "function_scan"
-	NamespaceActionEngine         = "action_engine"
-	NamespaceIdentityAccessReview = "identity_access_review"
-	NamespaceAutonomousWorkflow   = "autonomous_workflow"
-	NamespaceRuntimeIngest        = "runtime_ingest"
-	NamespaceProcessedCloudEvent  = "processed_cloud_event"
+	NamespacePlatformReportRun      = "report_run"
+	NamespaceWorkloadScan           = "workload_scan"
+	NamespaceImageScan              = "image_scan"
+	NamespaceFunctionScan           = "function_scan"
+	NamespaceActionEngine           = "action_engine"
+	NamespaceIdentityAccessReview   = "identity_access_review"
+	NamespaceAutonomousWorkflow     = "autonomous_workflow"
+	NamespaceRuntimeIngest          = "runtime_ingest"
+	NamespaceRuntimeReplay          = "runtime_replay"
+	NamespaceRuntimeMaterialization = "runtime_materialization"
+	NamespaceProcessedCloudEvent    = "processed_cloud_event"
 )
