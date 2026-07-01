6959a8f7dbc05587e4cc5dbd90b5f178ef41e6cc
Jonathan Haas <jonathan.haas@writer.com>
2026-07-01 15:09:33 -0700

    Remove unused Snyk config alias

--- sources/internal/snykapi/snykapi.go
+++ sources/internal/snykapi/snykapi.go
@@ -40,7 +40,6 @@ const (
 )
 
 const (
-	apiVersionConfig       = APIVersionConfig
 	orgIDConfig            = OrgIDConfig
 	groupIDConfig          = GroupIDConfig
 	assetIDConfig          = AssetIDConfig
