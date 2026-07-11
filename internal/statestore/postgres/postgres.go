f80d215b4ffe128fbdc17ebdab4be15d622200fb
Jonathan Haas <jonathan.haas@writer.com>
2026-07-11 07:42:48 -0700

    Persist compliance review projections

--- internal/statestore/postgres/postgres.go
+++ internal/statestore/postgres/postgres.go
@@ -46,6 +46,7 @@ type grcTablesReady struct {
 	customDashboards     bool
 	vendorDiscovery      bool
 	questionnaireRun     bool
+	complianceReview     bool
 }
 
 type appendLogTablesReady struct {
