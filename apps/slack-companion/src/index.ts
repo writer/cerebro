59ff469267eb29ea4401030e2fa7ef011137fe4c
Jonathan Haas <jonathan.haas@writer.com>
2026-07-16 07:18:51 -0700

    Add distributed work continuity

--- apps/slack-companion/src/index.ts
+++ apps/slack-companion/src/index.ts
@@ -4,7 +4,9 @@ export * from "./delivery/contracts.js";
 export * from "./delivery/coordinator.js";
 export * from "./delivery/ports.js";
 export * from "./distributed/contracts.js";
+export * from "./distributed/coordinator.js";
 export * from "./distributed/ports.js";
+export * from "./distributed/reference-store.js";
 export * from "./distributed/validation.js";
 export {
   ExecutionCoordinator,
